1b60800f4106adda35d60aa2d8c2df74d27dbb2c
Corentin Mimeau <corentin.mimeau@gmail.com>
2015-11-02 22:16:00 +0100

    Added based entity methods (constructor, create)

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,1 +1,18 @@
+#include "Entity.hpp"
 
+Entity::Entity()
+{
+
+}
+
+Entity::Entity(const std::string &name, const std::string &type, sf::Vector2f pos)
+{
+    create(name, type, pos);
+}
+
+void Entity::create(const std::string &name, const std::string &type, sf::Vector2f pos)
+{
+    m_name = name;
+    m_type = type;
+    m_position = pos;
+}
--- src/Entity.hpp
+++ src/Entity.hpp
@@ -1,13 +1,20 @@
 #ifndef H_ENTITY
 #define H_ENTITY
 
+#include <SFML/Graphics.hpp>
+#include <string>
+
 
 class Entity
 {
 public:
+    Entity();
+    Entity(const std::string &name, const std::string &type, sf::Vector2f pos);
+    void create(const std::string &name, const std::string &type, sf::Vector2f pos);
 
 private:
-
+    sf::Vector2f m_position;
+    std::string m_type, m_name;
 
 };
 
